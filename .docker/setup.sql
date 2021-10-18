8659b0aeab97d15b3c2c7b466bd8364ff93f8d09
loveyousomuch <loveyousomuch554@gmail.com>
2021-10-18 12:00:34 +0300

    sql tables

--- .docker/setup.sql
+++ .docker/setup.sql
@@ -1,5 +1,24 @@
-create table if not exists test (
-  id bigint generated always as identity primary key,
-  text_field varchar
+create table if not exists product (
+  product_id bigint generated always as identity primary key,
+  name varchar not null,
+  price numeric(15, 6) not null,
+  shelf_life int not null
+);
+
+create table if not exists provider (
+  provider_id bigint generated always as identity primary key,
+  name varchar not null,
+  INN varchar not null,
+  bank varchar not null,
+  checking_account varchar,
+  due_date int
+);
+
+create table if not exists batch (
+  batch_id bigint generated always as identity primary key,
+  date date, 
+  count bigint,
+  product_id bigint references product(product_id),
+  provider_id bigint references provider(provider_id)
 );
 
